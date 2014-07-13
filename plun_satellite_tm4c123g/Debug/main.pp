efdd8e4d5bda7079a9cc9809dd75edcb388a93c9
Byunghun Hwang <bhhwang@nsynapse.com>
2014-07-13 23:42:12 +0900

    edit

--- plun_satellite_tm4c123g/Debug/main.pp
+++ plun_satellite_tm4c123g/Debug/main.pp
@@ -1,5 +1,25 @@
 # FIXED
 
 main.obj: ../main.c
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdint.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdbool.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/yvals.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdarg.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/_lock.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
 
 ../main.c: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/_lock.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
