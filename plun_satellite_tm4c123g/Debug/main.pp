d2cdf36c11a6f891db26ad7cff5db4f905526c0b
bhhwang <bhhwang@nsynapse.com>
2014-07-14 23:49:48 +0900

    add cc3000 library

--- plun_satellite_tm4c123g/Debug/main.pp
+++ plun_satellite_tm4c123g/Debug/main.pp
@@ -14,6 +14,17 @@ main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/flash.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/eeprom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdarg.h
+main.obj: ../wlan.h
+main.obj: ../common/cc3000_common.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdlib.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/errno.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/time.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/cc3000/src/host_driver/core_driver/evnt_handler.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/cc3000/src/host_driver/core_driver/hci.h
 
 ../main.c: 
 c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdint.h: 
@@ -29,3 +40,14 @@ C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h:
 C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
 C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h: 
 C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/flash.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/eeprom.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdarg.h: 
+../wlan.h: 
+../common/cc3000_common.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/stdlib.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/errno.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.5/include/time.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/cc3000/src/host_driver/core_driver/evnt_handler.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/cc3000/src/host_driver/core_driver/hci.h: 
--- plun_satellite_tm4c123g/main.c
+++ plun_satellite_tm4c123g/main.c
@@ -1,11 +1,11 @@
 /*
- * plun satellite (TI tm4c123g) firmware for TI's TM4C123GH6PM Launchpad
+ * plun satellite (TI TM4C123G) firmware for TI's TM4C123GH6PM Launchpad
  * programmed by Byunghun Hwang <bhhwang@nsynapse.com>
  * Copyright (c) Nsynapse Inc. All rights reserved.
  * Version : 0.0.1
  * Date : 2014. 7. 14
  *
- * Note : This firmware can be used for TM4C123GH6PM Launchpad with CC3000 WiFi module.
+ * Note : This firmware can be used for TI's TM4C123GH6PM Launchpad with CC3000 WiFi boostpack.
  */
 
 
@@ -19,28 +19,55 @@
 #include <driverlib/interrupt.h>
 #include <driverlib/gpio.h>
 #include <driverlib/timer.h>
+#include <driverlib/flash.h>
+#include <driverlib/eeprom.h>
+#include <utils/uartstdio.h>
 
+
+#include "wlan.h"
+#include <cc3000/src/host_driver/core_driver/evnt_handler.h>
+#include <nvmem.h>
+#include "common/cc3000_common.h"
+#include <netapp.h>
+#include <cc3000/src/spi/spi.h>
+#include "common/hci.h"
+#include <cc3000/src/spi/spi_version.h>
+#include <cc3000/drivers/dk-tm4c123g/board.h>
+#include <host_driver_version.h>
+#include <security.h>
+
+#define	LED_GREEN	GPIO_PORTF_BASE, GPIO_PIN_3
+#define LED_BLUE	GPIO_PORTF_BASE, GPIO_PIN_2
+#define LED_RED		GPIO_PORTF_BASE, GPIO_PIN_1
+#define LED_IND		GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+
+
+const char ssid[] = "nsynapse";
+const char pass[] = "ghkdqudgns";
+
+
+
+/*
+ * Timer0 Interrupt handler
+ * @remark	must set this function into the startup_css.c file(vector table)
+ */
 void Timer0IntHandler(void)
 {
-	// Clear the timer interrupt
+	/*
+	 * Clear Timer0
+	 */
 	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-	// Read the current state of the GPIO pin and
-	// write back the opposite state
-	if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2))
-	{
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-	}
+	if(GPIOPinRead(LED_GREEN))
+		GPIOPinWrite(LED_IND, 0);
 	else
-	{
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 4);
-	}
+		GPIOPinWrite(LED_GREEN, GPIO_PIN_3);
 }
 
 void setup()
 {
 	/*
-	 * use main oscillator(16MHz) + use PLL(400MHz) + devide by 4 : system clock = 50MHz
+	 * use main oscillator(16MHz) + use PLL(400MHz) + devided by 4 + devided by 2(default) : system clock = 50MHz
 	 */
 	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
@@ -51,19 +78,24 @@ void setup()
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
 
 	/*
-	 * Timer 0
+	 * Timer 0 (periodic)
 	 */
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 	TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
 
-	uint32_t ui32Period;
-	ui32Period = SysCtlClockGet();
+
+	/*
+	 * timer 0 period set to 500ms
+	 */
+	uint32_t ui32Period = SysCtlClockGet()/2;
 	TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period -1);
 
+	/*
+	 * Enable interrupt
+	 */
 	IntEnable(INT_TIMER0A);
 	TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 	IntMasterEnable();
-
 	TimerEnable(TIMER0_BASE, TIMER_A);
 }
 
